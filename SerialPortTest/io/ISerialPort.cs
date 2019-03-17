40f679445d25c6e864302311a613900b09c8dcd1
camilin87 <camilin87@gmail.com>
2019-03-16 23:19:21 -0400

    start creating a wrapper around the serial port

--- SerialPortTest/io/ISerialPort.cs
+++ SerialPortTest/io/ISerialPort.cs
@@ -6,7 +6,7 @@ using System.Threading.Tasks;
 
 namespace SerialPortTest.io
 {
-    interface ISerialPort
+    public interface ISerialPort : IDisposable
     {
         void Send(byte[] data);
     }
---
+++ SerialPortTest/io/SerialPortReal.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.IO.Ports;
+using System.Text;
+
+namespace SerialPortTest.io
+{
+    public class SerialPortReal : ISerialPort
+    {
+        private const int MAX_CAPACITY = 10 * 1024;
+        private readonly SerialPort serialPort;
+
+        private readonly object bufferCriticalSection = new object();
+        private readonly StringBuilder receivedDataBuffer = new StringBuilder(MAX_CAPACITY);
+
+        public SerialPortReal(string port)
+        {
+            serialPort = new SerialPort(port);
+            serialPort.DataReceived += SerialPort_DataReceived;
+        }
+
+        private void SerialPort_DataReceived(object sender, SerialDataReceivedEventArgs e)
+        {
+            //serialPort.ReadExisting()
+        }
+
+        public void Send(byte[] data)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Dispose()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
